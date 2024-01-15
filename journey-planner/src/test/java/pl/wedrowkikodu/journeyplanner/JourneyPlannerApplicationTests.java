8dc313b81232aee500e07e20a9ae9aaa5c952521
Rafał Boszkowicz <rafal.boszkowicz@gmail.com>
2024-01-15 21:39:37 +0100

    Init commit

---
+++ journey-planner/src/test/java/pl/wedrowkikodu/journeyplanner/JourneyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wedrowkikodu.journeyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
